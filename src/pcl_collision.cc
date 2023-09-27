e0357516722c3cf4f512364cd505180eea28142b
D_ming <188461831163@163.com>
2023-09-27 17:55:18 +0800

    add vis_ros1 and class

--- src/pcl_collision.cc
+++ src/pcl_collision.cc
@@ -8,7 +8,8 @@
 #include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/PoseStamped.h>
-
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <Eigen/Core>
 
 #include <iostream>
@@ -17,158 +18,150 @@
 #include <cmath>
 
 #include "../include/sdqp.hpp"
+#include "vis_ros1.hpp"
 
-using Ev2 = Eigen::Vector2d;
-ros::Publisher pcl_pub;
-ros::Publisher robot_pub;
-ros::Publisher arrow_pub;
-Ev2 robot_center;
+using Vec2d = Eigen::Vector2d;
 
-pcl::PointCloud<pcl::PointXYZRGB> cloud;
-std::vector<Ev2> cloud_lists;
-void pointcloudGeneration(double mu, double sigma, int _width = 10, int _height = 1)
+class collision_vec
 {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::normal_distribution<double> random_x(mu, sigma), random_y(mu, sigma);
-    cloud.width = _width;
-    cloud.height = _height;
-    cloud.points.resize(cloud.width * cloud.height);
-    for (int i = 0; i < cloud.points.size(); i++)
-    {
-        cloud.points[i].x = random_x(gen);
-        cloud.points[i].y = random_y(gen);
-        cloud.points[i].z = 0;
-        cloud.points[i].r = 255 * (i + 10) / (cloud.points.size() + 10);
-        cloud.points[i].g = 0;
-        cloud.points[i].b = 0;
-        cloud_lists.emplace_back(Ev2(cloud.points[i].x, cloud.points[i].y));
-        std::cout << "x=" << cloud.points[i].x;
-    }
-}
 
-void robotCallBack(const geometry_msgs::PoseStamped::ConstPtr &msg)
-{
-    robot_center = Ev2(msg->pose.position.x, msg->pose.position.y);
-    if (cloud_lists.empty())
+public:
+    void Init(ros::NodeHandle &nh)
     {
-        ROS_WARN("NO POINT CLOUD");
-        return;
-    }
-    // sdqp problem
-    int m = 2;
-    Ev2 z = Ev2::Zero();
-    Eigen::Matrix2d Q = Eigen::Matrix2d::Identity();
-    Ev2 c = Ev2::Zero();
-    Eigen::MatrixXd A; // TODO
-    A.resize(cloud_lists.size(), 2);
-    for (int i = 0; i < A.rows(); i++)
-    {
-        A.row(i) = (robot_center - cloud_lists[i]).transpose();
-    }
-    Eigen::VectorXd b = -1.0 * Eigen::VectorXd::Ones(cloud_lists.size());
-    double cost = sdqp::sdqp<2>(Q, c, A, b, z);
-    Ev2 x;
-    ROS_INFO_STREAM("___--------------____");
-    if (cost != INFINITY)
-    {
-        x = z / z.squaredNorm() + robot_center;
-        std::cout << "optimal z: " << z.transpose() << std::endl;
-        std::cout << "final cost: " << cost << std::endl;
-        std::cout << "collision vector: " << x.transpose() << std::endl;
-        std::cout << "collision distance: " << x.norm() << std::endl;
-    }
-    else
-    {
-        ROS_WARN("SDQP has not feasiable solution. Robot collided with an obstacle !");
-    }
-    // robot center
-    visualization_msgs::Marker robot;
-    robot.header.frame_id = "point_cloud_frame_id";
-    robot.header.stamp = msg->header.stamp;
-    robot.id = 0;
-    robot.type = visualization_msgs::Marker::SPHERE;
-    robot.action = visualization_msgs::Marker::ADD;
-    robot.ns = "robot_center";
-    robot.pose.position.x = robot_center.x();
-    robot.pose.position.y = robot_center.y();
-    robot.pose.position.z = 0.0;
-    robot.pose.orientation.x = 0.0;
-    robot.pose.orientation.y = 0.0;
-    robot.pose.orientation.z = 0.0;
-    robot.pose.orientation.w = 1.0;
-    robot.scale.x = 0.2;
-    robot.scale.y = 0.2;
-    robot.scale.z = 0.2;
-    robot.color.a = 1.0;
-    robot.color.b = 0.0;
-    robot.color.g = 1.0;
-    robot.color.r = 0.0;
-    robot_pub.publish(robot);
+        nh_ = nh;
+        vis_ptr_.reset(new vis::displayRviz(nh_));
+        vis_ptr_->enroll<vis::vMarker>("collision_arrow");
+        vis_ptr_->enroll<vis::vMarker>("robot_center");
+        pcl_pub = nh_.advertise<sensor_msgs::PointCloud2>("pcl_output", 1);
 
-    // collision arrow
-    if (cost != INFINITY)
-    {
-        visualization_msgs::Marker arrow;
-        arrow.header.frame_id = "point_cloud_frame_id";
-        arrow.header.stamp = msg->header.stamp;
-        arrow.id = 0;
-        arrow.action = visualization_msgs::Marker::ADD;
-        arrow.ns = "collisin_arrow";
-        arrow.scale.x = 0.1;
-        arrow.scale.y = 0.1;
-        arrow.scale.z = 0.5;
-        arrow.color.a = 1.0;
-        arrow.color.b = 1.0;
-        arrow.color.g = 0.0;
-        arrow.color.r = 0.0;
-        geometry_msgs::Point p1, p2;
-        p1.x = robot_center.x();
-        p1.y = robot_center.y();
-        p1.z = 0.0;
-        p2.x = x.x();
-        p2.y = x.y();
-        p2.z = 0.0;
-        arrow.points.push_back(p1);
-        arrow.points.push_back(p2);
-        arrow_pub.publish(arrow);
+        robot_pub = nh_.advertise<visualization_msgs::Marker>(
+            "robot_center", 1, true);
+        arrow_pub = nh_.advertise<visualization_msgs::Marker>(
+            "collision_arrow", 1, true);
+        ros::Subscriber robot_sub = nh_.subscribe("/move_base_simple/goal", 1, &collision_vec::robotCallBack, this);
+        pointcloudGeneration(0, 3, 30);
+
+        pcl::toROSMsg(cloud, output_point_msg);
+        output_point_msg.header.frame_id = "map";
+        ros::Rate loop_rate(11);
+
+        while (ros::ok())
+        {
+            output_point_msg.header.stamp = ros::Time::now();
+            pcl_pub.publish(output_point_msg);
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
     }
-    else
+    void run()
     {
-        visualization_msgs::Marker arrow;
-        arrow.header.frame_id = "point_cloud_frame_id";
-        arrow.header.stamp = msg->header.stamp;
-        arrow.id = 0;
-        arrow.ns = "collisin_arrow";
-        arrow.action = visualization_msgs::Marker::DELETE;
-        arrow.color.a = 0.0;
-        arrow_pub.publish(arrow);
     }
-}
+
+private:
+    ros::NodeHandle nh_;
+    ros::Publisher pcl_pub;
+    ros::Publisher robot_pub;
+    ros::Publisher arrow_pub;
+    Vec2d robot_center;
+    std::shared_ptr<vis::displayRviz> vis_ptr_;
+    pcl::PointCloud<pcl::PointXYZRGB> cloud; // random point cloud
+    sensor_msgs::PointCloud2 output_point_msg;
+    std::vector<Vec2d> cloud_lists;
+
+    void pointcloudGeneration(double mu, double sigma, int _width = 10, int _height = 1)
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::normal_distribution<double> random_x(mu, sigma), random_y(mu, sigma);
+        cloud.width = _width;
+        cloud.height = _height;
+        cloud.points.resize(cloud.width * cloud.height);
+        for (int i = 0; i < cloud.points.size(); i++)
+        {
+            cloud.points[i].x = random_x(gen);
+            cloud.points[i].y = random_y(gen);
+            cloud.points[i].z = 0;
+            cloud.points[i].r = 255 * (i + 10) / (cloud.points.size() + 10);
+            cloud.points[i].g = 0;
+            cloud.points[i].b = 0;
+            cloud_lists.emplace_back(Vec2d(cloud.points[i].x, cloud.points[i].y));
+            std::cout << "x=" << cloud.points[i].x;
+        }
+    }
+
+    void robotCallBack(const geometry_msgs::PoseStamped::ConstPtr &msg)
+    {
+        ROS_INFO("robotCallback");
+        robot_center = Vec2d(msg->pose.position.x, msg->pose.position.y);
+        if (cloud_lists.empty())
+        {
+            ROS_WARN("NO POINT CLOUD");
+            return;
+        }
+        // sdqp problem
+        int m = 2;
+        Vec2d z = Vec2d::Zero();
+        Eigen::Matrix2d Q = Eigen::Matrix2d::Identity();
+        Vec2d c = Vec2d::Zero();
+        Eigen::MatrixXd A; // TODO
+        A.resize(cloud_lists.size(), 2);
+        for (int i = 0; i < A.rows(); i++)
+        {
+            A.row(i) = (robot_center - cloud_lists[i]).transpose();
+        }
+        Eigen::VectorXd b = -1.0 * Eigen::VectorXd::Ones(cloud_lists.size());
+        double cost = sdqp::sdqp<2>(Q, c, A, b, z);
+        Vec2d x;
+        ROS_INFO_STREAM("___--------------____");
+        if (cost != INFINITY)
+        {
+            x = z / z.squaredNorm() + robot_center;
+            std::cout << "optimal z: " << z.transpose() << std::endl;
+            std::cout << "final cost: " << cost << std::endl;
+            std::cout << "collision vector: " << x.transpose() << std::endl;
+            std::cout << "collision distance: " << x.norm() << std::endl;
+        }
+        else
+        {
+            ROS_WARN("SDQP has not feasiable solution. Robot collided with an obstacle !");
+        }
+        // robot center
+        geometry_msgs::Point robot_pt;
+        robot_pt.x=robot_center(0);
+        robot_pt.y=robot_center(1);
+        robot_pt.z=0.0;//robot_center(2);
+        vis_ptr_->vis_single_marker(robot_pt,"robot_center",Vec3d(0.1,0.1,0.3),vis::vMarker::CUBE,"map",vis::pink);
+
+
+        // collision arrow
+        if (cost != INFINITY)
+        {
+            geometry_msgs::Point p1, p2;
+            p1.x = robot_center.x();
+            p1.y = robot_center.y();
+            p1.z = 0.0;
+            p2.x = x.x();
+            p2.y = x.y();
+            p2.z = 0.0;
+            vis_ptr_->vis_arrow(p1, p2, "collision_arrow", Vec3d(0.1, 0.1, 0.6),vis::vMarker::ADD,"map",vis::orange); //,vis::blue
+            
+        }
+        else
+        {
+            geometry_msgs::Point p1, p2;
+            vis_ptr_->vis_arrow(p1, p2, "collision_arrow", Vec3d(0.1, 0.1, 0.6),vis::vMarker::DELETE);
+
+        }
+    }
+};
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "pcl_create");
 
     ros::NodeHandle nh;
-    pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("pcl_output", 1);
-
-    robot_pub = nh.advertise<visualization_msgs::Marker>(
-        "robot_center", 1, true);
-    arrow_pub = nh.advertise<visualization_msgs::Marker>(
-        "collision_arrow", 1, true);
-    ros::Subscriber robot_sub = nh.subscribe("/move_base_simple/goal", 1, robotCallBack);
-    pointcloudGeneration(0, 3, 30);
-    sensor_msgs::PointCloud2 output_point_msg;
-    pcl::toROSMsg(cloud, output_point_msg);
-    output_point_msg.header.frame_id = "point_cloud_frame_id";
-
-    ros::Rate loop_rate(1);
-    while (ros::ok())
-    {
-        pcl_pub.publish(output_point_msg);
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-
+    collision_vec cv;
+    cv.Init(nh);
+    // cv.run();
     return 0;
 }
---
+++ src/vis_ros1.hpp
@@ -0,0 +1,366 @@
+#ifndef VIS_ROS1
+#define VIS_ROS1
+#include <nav_msgs/Path.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/Marker.h>
+
+#include <Eigen/Geometry>
+#include <iostream>
+#include <unordered_map>
+
+using Vec2d = Eigen::Vector2d;
+using Vec2ds = std::vector<Vec2d>;
+using Vec2i = Eigen::Vector2i;
+
+using Vec3d = Eigen::Vector3d;
+using Vec3ds = std::vector<Eigen::Vector3d>;
+
+using Vec4d = Eigen::Vector4d;
+using Vec4ds = std::vector<Eigen::Vector4d>;
+
+using VecXd = Eigen::VectorXd;
+
+using Matxd = Eigen::MatrixXd;
+
+using PointType = pcl::PointXYZI;
+
+namespace vis
+{
+    using vMarkers = visualization_msgs::MarkerArray;
+    using vMarker = visualization_msgs::Marker;
+
+    enum Color
+    {
+        white,
+        red,
+        green,
+        blue,
+        yellow,
+        chartreuse,
+        black,
+        gray,
+        orange,
+        purple,
+        pink,
+        steelblue
+    };
+
+    class displayRviz
+    {
+
+        ros::NodeHandle nh_;
+        std::unordered_map<std::string, ros::Publisher> vis_pub_map_;
+
+        void setMarkerPose(visualization_msgs::Marker &marker,
+                           const double &x,
+                           const double &y,
+                           const double &z)
+        {
+            marker.pose.position.x = x;
+            marker.pose.position.y = y;
+            marker.pose.position.z = z;
+            marker.pose.orientation.w = 1;
+            marker.pose.orientation.x = 0;
+            marker.pose.orientation.y = 0;
+            marker.pose.orientation.z = 0;
+        }
+
+        void setMarkerColor(visualization_msgs::Marker &marker,
+                            Color color = blue,
+                            double a = 1)
+        {
+            marker.color.a = a;
+            switch (color)
+            {
+            case white:
+                marker.color.r = 1;
+                marker.color.g = 1;
+                marker.color.b = 1;
+                break;
+            case red:
+                marker.color.r = 1;
+                marker.color.g = 0;
+                marker.color.b = 0;
+                break;
+            case green:
+                marker.color.r = 0;
+                marker.color.g = 1;
+                marker.color.b = 0;
+                break;
+            case blue:
+                marker.color.r = 0;
+                marker.color.g = 0;
+                marker.color.b = 1;
+                break;
+            case yellow:
+                marker.color.r = 1;
+                marker.color.g = 1;
+                marker.color.b = 0;
+                break;
+            case chartreuse:
+                marker.color.r = 0.5;
+                marker.color.g = 1;
+                marker.color.b = 0;
+                break;
+            case black:
+                marker.color.r = 0;
+                marker.color.g = 0;
+                marker.color.b = 0;
+                break;
+            case gray:
+                marker.color.r = 0.5;
+                marker.color.g = 0.5;
+                marker.color.b = 0.5;
+                break;
+            case orange:
+                marker.color.r = 1;
+                marker.color.g = 0.5;
+                marker.color.b = 0;
+                break;
+            case purple:
+                marker.color.r = 0.5;
+                marker.color.g = 0;
+                marker.color.b = 1;
+                break;
+            case pink:
+                marker.color.r = 1;
+                marker.color.g = 0;
+                marker.color.b = 0.6;
+                break;
+            case steelblue:
+                marker.color.r = 0.4;
+                marker.color.g = 0.7;
+                marker.color.b = 1;
+                break;
+            }
+        }
+        void setMarkerScale(visualization_msgs::Marker &marker,
+                            const double &x,
+                            const double &y,
+                            const double &z)
+        {
+            marker.scale.x = x;
+            marker.scale.y = y;
+            marker.scale.z = z;
+        }
+        void setMarkerScale(visualization_msgs::Marker &marker,
+                            const Vec3d &scale_xyz)
+        {
+            marker.scale.x = scale_xyz(0);
+            marker.scale.y = scale_xyz(1);
+            marker.scale.z = scale_xyz(2);
+        }
+
+    public:
+        ~displayRviz(){};
+        displayRviz(ros::NodeHandle &nh) : nh_(nh){
+
+                                           };
+        template <class _pub_type, class _topic>
+        void enroll(const _topic &topic)
+        {
+            std::unordered_map<std::string, ros::Publisher>::iterator pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<_pub_type>(topic, 8);
+                vis_pub_map_[topic] = vis_pub;
+            }
+        }
+        template <class _pub_type, class _topic>
+        void enroll(const _topic &topic, uint32_t queue_size)
+        {
+            std::unordered_map<std::string, ros::Publisher>::iterator pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<_pub_type>(topic, queue_size);
+                vis_pub_map_[topic] = vis_pub;
+            }
+        }
+        //
+        template <class _path, class _topic>
+        void vis_path(const _path &path, const _topic &topic, const std::string &frame = "map")
+        {
+            // TODO note whether to enroll
+            auto pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<nav_msgs::Path>(topic, 8);
+                vis_pub_map_[topic] = vis_pub;
+            }
+            const std::string frame_path = frame;
+            nav_msgs::Path tmp_path;
+            geometry_msgs::PoseStamped tmp_poses;
+            // geometry_msgs::Pose tmp_pose;
+            tmp_poses.header.frame_id = frame_path;
+            for (const auto &pt : path)
+            {
+                tmp_poses.pose.position.x = pt[0];
+                tmp_poses.pose.position.y = pt[1];
+                tmp_poses.pose.position.z = pt[2];
+                tmp_path.poses.emplace_back(tmp_poses);
+            }
+            tmp_path.header.frame_id = frame_path;
+            tmp_path.header.stamp = ros::Time::now();
+            vis_pub_map_[topic].publish(tmp_path);
+        }
+        template <class _point, class _topic>
+        void vis_pointcloud(const _point &ptc, const _topic &topic, const std::string &frame = "map")
+        {
+            auto pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<sensor_msgs::PointCloud2>(topic, 8);
+                vis_pub_map_[topic] = vis_pub;
+            }
+            pcl::PointCloud<pcl::PointXYZ> point_cloud;
+            sensor_msgs::PointCloud2 point_cloud_msg;
+            point_cloud.reserve(ptc.size());
+            for (const auto &pt : ptc)
+            {
+                point_cloud.points.emplace_back(pt[0], pt[1], pt[2]);
+            }
+            pcl::toROSMsg(point_cloud, point_cloud_msg);
+            point_cloud_msg.header.frame_id = frame;
+            point_cloud_msg.header.stamp = ros::Time::now();
+            vis_pub_map_[topic].publish(point_cloud_msg);
+        }
+
+        template <class _point, class _topic>
+        void vis_pointcloud_intensity(const _point &ptc, const _topic &topic, const std::string &frame = "map")
+        {
+            auto pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<sensor_msgs::PointCloud2>(topic, 8);
+                vis_pub_map_[topic] = vis_pub;
+            }
+            pcl::PointCloud<pcl::PointXYZI> point_cloud;
+            sensor_msgs::PointCloud2 point_cloud_msg;
+            point_cloud.reserve(ptc.size());
+            for (const auto &pt : ptc)
+            {
+                point_cloud.points.emplace_back(pt[0], pt[1], pt[2]);
+            }
+            pcl::toROSMsg(point_cloud, point_cloud_msg);
+            point_cloud_msg.header.frame_id = frame;
+            point_cloud_msg.header.stamp = ros::Time::now();
+            vis_pub_map_[topic].publish(point_cloud_msg);
+        }
+        // display point_cloud directly
+        template <class _cloud, class _topic>
+        void vis_pcl(const _cloud &ptc, const _topic &topic, const std::string &frame = "map")
+        {
+            auto pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<sensor_msgs::PointCloud2>(topic, 8);
+                vis_pub_map_[topic] = vis_pub;
+            }
+            sensor_msgs::PointCloud2 point_cloud_msg;
+            pcl::toROSMsg(ptc, point_cloud_msg);
+            point_cloud_msg.header.frame_id = frame;
+            point_cloud_msg.header.stamp = ros::Time::now();
+            vis_pub_map_[topic].publish(point_cloud_msg);
+        }
+
+        template <class _path_point, class _topic>
+        void vis_path_point(const _path_point &path_point, const _topic &topic, const std::string &frame = "map")
+        {
+
+            vis_path(path_point, topic);
+            vis_pointcloud(path_point, std::string(topic) + "cloud");
+        }
+
+        template <class _path_point, class _topic>
+        void vis_path_strip(const _path_point &path_point, const _topic &topic, const std::string &frame = "map", double vscale = 0.03, const Color color = red)
+        {
+
+            auto pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<vMarkers>(topic, 9);
+                vis_pub_map_[topic] = vis_pub;
+            }
+
+            vMarkers path_strip_msg;
+            vMarker path_msg;
+            path_strip_msg.markers.reserve(path_point.size() + 1);
+            // only display the current path
+            // vMarker clear_previous_msg;
+            // clear_previous_msg.action = vMarker::DELETEALL;
+            // path_strip_msg.markers.emplace_back(clear_previous_msg);
+            path_msg.type = vMarker::LINE_STRIP;
+            path_msg.action = vMarker::ADD;
+            path_msg.header.frame_id = frame;
+            path_msg.id = 0;
+            setMarkerPose(path_msg, 0, 0, 0);
+            setMarkerScale(path_msg, vscale, vscale, vscale);
+            setMarkerColor(path_msg, color);
+
+            for (const auto &path : path_point)
+            {
+
+                path_msg.points.resize(path.size());
+                for (size_t i = 0; i < path.size(); ++i)
+                {
+                    ROS_INFO_STREAM("X: " << path[i].x() << " Y: " << path[i].y());
+                    path_msg.points[i].x = path[i].x();
+                    path_msg.points[i].y = path[i].y();
+                    path_msg.points[i].z = path[i].z();
+                }
+                path_strip_msg.markers.emplace_back(path_msg);
+                ++path_msg.id;
+            }
+            vis_pub_map_[topic].publish(path_strip_msg);
+        }
+
+        template <class _topic>
+        void vis_arrow(const geometry_msgs::Point &st, const geometry_msgs::Point end, const _topic &topic,
+                       const Vec3d &scale_xyz, const int32_t action_type = vMarker::ADD, const std::string &frame = "map", const Color color = red)
+        {
+            auto pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<vMarker>(topic, 1);
+                vis_pub_map_[topic] = vis_pub;
+            }
+            vMarker arrow;
+            arrow.header.frame_id = frame;
+            arrow.header.stamp = ros::Time::now();
+            arrow.id = 0;
+            arrow.action = action_type;
+            arrow.ns = "arrow";
+            setMarkerScale(arrow, scale_xyz);
+            setMarkerColor(arrow, color, 0.9);
+            arrow.points.emplace_back(st);
+            arrow.points.emplace_back(end);
+            vis_pub_map_[topic].publish(arrow);
+        }
+        template <class _topic>
+        void vis_single_marker(const geometry_msgs::Point pt, const _topic &topic,
+                               const Vec3d &scale_xyz, int32_t marker_type = vMarker::SPHERE, const std::string &frame = "map", const Color color = red)
+        {
+            auto pub_iter = vis_pub_map_.find(topic);
+            if (pub_iter == vis_pub_map_.end())
+            {
+                ros::Publisher vis_pub = nh_.advertise<vMarker>(topic, 1);
+                vis_pub_map_[topic] = vis_pub;
+            }
+            vMarker single_marker;
+            single_marker.header.frame_id = frame;
+            single_marker.header.stamp = ros::Time::now();
+            single_marker.id = 0;
+            single_marker.type = marker_type;
+            single_marker.action = vMarker::ADD;
+            setMarkerPose(single_marker, pt.x, pt.y, pt.z);
+            setMarkerColor(single_marker, color);
+            setMarkerScale(single_marker, scale_xyz);
+            vis_pub_map_[topic].publish(single_marker);
+        }
+    };
+
+}
+#endif
